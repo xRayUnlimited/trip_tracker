cbd83f59a66bc5f27198d2f0f0230f5116efc3d1
Ray Suyoto <ray.suy@icloud.com>
2018-03-09 14:09:19 -0700

    begin trip

--- client/src/components/LocationList.js
+++ client/src/components/LocationList.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import axios from 'axios';
+// import React from 'react';
+// import axios from 'axios';
 
-class LocationList extends React.Component {
-  state = { 
+// class LocationList extends React.Component {
+//   state = {}
 
-  }
-  render(){
-    return(
-      <div> 
-        <h3>{name}</h3>
-        <h5>{this.props.address}</h5>
-      </div>
-    )
-  }
-}
-
-export default LocationList;
+// export default LocationList;
--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 
-const Trip = ({...props}) => (
-    <p>Trip stuff goes here, 
-    call it in programatically instead of manually
-    the way we have it set up now conflicts,
-    current logical way makes it so that the trip list shows up
-    where it's called. 
-    </p>
+class Trip extends React.Component {
+    render() {
+      return(
+        <div>
+        <h3>Trips></h3>
+        
+        </div>
+      )
+      }
+    }
 
-
-);
-
-//
 
 export default Trip;
